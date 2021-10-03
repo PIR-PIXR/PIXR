5477cdcc7f0793b62fec2003a414024ad48ba85b
QUANG <87842051+cnquang@users.noreply.github.com>
2021-10-03 21:42:13 +0700

    Building a real Merkle tree in Bitcoin live.

--- src/main/java/au/edu/rmit/blockchain/crypto/pir/PIR.java
+++ src/main/java/au/edu/rmit/blockchain/crypto/pir/PIR.java
@@ -4,7 +4,6 @@ import au.edu.rmit.blockchain.crypto.common.api.BlockChainApi;
 import au.edu.rmit.blockchain.crypto.common.dto.Block;
 import au.edu.rmit.blockchain.crypto.common.dto.LatestBlock;
 import au.edu.rmit.blockchain.crypto.common.dto.Transaction;
-import au.edu.rmit.blockchain.crypto.common.utils.Util;
 import au.edu.rmit.blockchain.crypto.common.utils.http.APIException;
 import au.edu.rmit.blockchain.crypto.pir.algorithms.MerkleTrees;
 
@@ -19,42 +18,35 @@ public class PIR {
 
         //Latest Block
         LatestBlock latestBlock = api.getLatestBlock();
-        System.out.println("Latest Block: " + latestBlock.getHash());
+        System.out.println("Block hash: " + latestBlock.getHash());
 
         //Single Block
         Block sblock = api.getBlock(latestBlock.getHash());
+        //Block sblock = api.getBlock("0000000000000000e067a478024addfecdc93628978aa52d91fabd4292982a50");
 
-        //Hash Map: key = tx_index; value = tx hash
-        TreeMap<Long, String> txHash = toTreeMap(sblock.getTransactions());
+        System.out.println("Merkle root: " + sblock.getMerkleRoot());
+        System.out.println("Number of transactions: " + sblock.getTransactions().size());
 
-        System.out.println("merkle root: " + sblock.getMerkleRoot());
-        System.out.println("number of transactions: " + sblock.getTransactions().size());
+        //Ordering txs in the Merkle tree
+        List<String> txHashList = toList(sblock.getTransactions());
 
-        //  # of leaves 2^x - full MerkleTree
-        long size = (long) Math.pow(2, Util.log2(txHash.size()) + 1);
-        System.out.println("test : " + size);
-
-        List<String> tempTxList = new ArrayList<>(txHash.values());
-        MerkleTrees merkleTrees = new MerkleTrees(tempTxList);
-        merkleTrees.merkle_tree(size);
-        System.out.println("root : " + merkleTrees.getRoot());
-
-        //Test Merkle tree with 4 transaction hashes
-        List<String> testTxList = new ArrayList<>();
-        testTxList.add("b7dc35d70edd340636dee8995d542008a1f149015ec37804d48c9cf21f31d5c6");
-        testTxList.add("685865651c0928c891a1ef42f8613b92e7741df2696debbf6aaf0732eabd06b6");
-        testTxList.add("57e2ca5c42caf7f0d29c9e9105e918150ff74a34005919a27aa1f059b7ba15dc");
-        testTxList.add("5836e7095761dcfa3fd998b900feff31e41f22d0f6f0a1e629cb9f7e29db7706");
-
-        MerkleTrees MT = new MerkleTrees(testTxList);
+        MerkleTrees MT = new MerkleTrees(txHashList);
         MT.merkle_tree();
-        System.out.println("Root test : " + MT.getRoot());
+        System.out.println("Root result : " + MT.getRoot());
     }
 
     private static TreeMap<Long, String> toTreeMap(List<Transaction> txs) {
         TreeMap<Long, String> txH = new TreeMap<>();
         for (Transaction tx : txs) {
             txH.put(tx.getIndex(), tx.getHash());
+        }
+        return txH;
+    }
+
+    private static List<String> toList(List<Transaction> txs) {
+        List<String> txH = new ArrayList<>();
+        for (Transaction tx : txs) {
+            txH.add(tx.getHash());
         }
         return txH;
     }
--- src/main/java/au/edu/rmit/blockchain/crypto/pir/algorithms/MerkleTrees.java
+++ src/main/java/au/edu/rmit/blockchain/crypto/pir/algorithms/MerkleTrees.java
@@ -2,105 +2,80 @@ package au.edu.rmit.blockchain.crypto.pir.algorithms;
 
 import java.security.MessageDigest;
 import java.util.*;
- 
+
 public class MerkleTrees {
-	
+    //List of transactions
     private List<String> txList;
     // Merkle Root
     private String root;
-    
+
     //constructor
     public MerkleTrees(List<String> txList) {
         this.txList = txList;
         root = "";
     }
- 
-    /**
-     * execute full merkle_tree and set root.
-     */
-    public void merkle_tree(long size) {
- 
-        List<String> tempTxList = new ArrayList<String>();
- 
-        for (int i = 0; i < size; i++) {
-        	if (i < this.txList.size()) {
-        		tempTxList.add(this.txList.get(i));
-        	}
-        	else tempTxList.add("");
-        }
- 
-        List<String> newTxList = getNewTxList(tempTxList);
- 
-        //Execute the loop until only one hash value is left
-        while (newTxList.size() != 1) {
-            newTxList = getNewTxList(newTxList);
-        }
- 
-        this.root = newTxList.get(0);
-    }
-    
+
     /**
      * execute full merkle_tree and set root.
      */
     public void merkle_tree() {
- 
-        List<String> tempTxList = new ArrayList<String>();
- 
-        for (int i = 0; i < this.txList.size(); i++) {
-        	tempTxList.add(this.txList.get(i));
-        }
- 
+        List<String> tempTxList = new ArrayList<>(txList);
+
         List<String> newTxList = getNewTxList(tempTxList);
- 
+
         //Execute the loop until only one hash value is left
         while (newTxList.size() != 1) {
             newTxList = getNewTxList(newTxList);
         }
- 
+
         this.root = newTxList.get(0);
     }
- 
+
     /**
-     * return Node Hash List.
-     * @param tempTxList
-     * @return
+     * return New Hash List.
      */
     private List<String> getNewTxList(List<String> tempTxList) {
- 
-        List<String> newTxList = new ArrayList<String>();
-        int index = 0;
-        while (index < tempTxList.size()) {
-            // left
-            String left = tempTxList.get(index);
-            index++;
-            // right
-            String right = "";
-            if (index != tempTxList.size()) {
-                right = tempTxList.get(index);
-            }
-            // sha2 hex value
-            String sha2HexValue = getSHA2HexValue(left + right);
-            newTxList.add(sha2HexValue);
-            index++;
+
+        List<String> newTxList = new ArrayList<>();
+        //hashing range of pairs nodes
+        for (int i = 0; i < tempTxList.size() - 1; i = i + 2){
+            String parentHash = rStrInHex(getSHA256(tempTxList.get(i), tempTxList.get(i + 1)));
+            newTxList.add(parentHash);
         }
- 
+        //hashing the final odd node if appeared
+        if (tempTxList.size() % 2 == 1) {
+            // sha2 hex value
+            String parentHash = rStrInHex(getSHA256(tempTxList.get(tempTxList.size() - 1), tempTxList.get(tempTxList.size() - 1)));
+            newTxList.add(parentHash);
+            //System.out.println("odd: " + parentHash);
+        }
+
         return newTxList;
     }
- 
+
     /**
-     * Return hex string
-     * @param str
-     * @return
+     * Return hash value from two leaves
      */
-    public String getSHA2HexValue(String str) {
-        byte[] cipher_byte;
+    public String getSHA256(String strInHexLeft, String strInHexRight) {
         try{
+            //System.out.println("input: " + strInHex);
             MessageDigest md = MessageDigest.getInstance("SHA-256");
-            md.update(str.getBytes());
-            cipher_byte = md.digest();
-            StringBuilder sb = new StringBuilder(2 * cipher_byte.length);
-            for(byte b: cipher_byte) {
-                sb.append(String.format("%02x", b&0xff) );
+
+            String rStrLeftInHex = rStrInHex(strInHexLeft);
+            String rStrRightInHex = rStrInHex(strInHexRight);
+            String rStrcombine = rStrLeftInHex + rStrRightInHex;
+
+            byte[] rStrInByte;
+            rStrInByte = StrInByte(rStrcombine);
+
+            //hashing a string in Hex value (sha256)
+            byte[] digest = md.digest(md.digest(rStrInByte));
+
+            //Convert Hex to String
+            //bytes to hex
+            StringBuilder sb = new StringBuilder(2 * digest.length);
+            for(byte b: digest) {
+                sb.append(String.format("%02x", b) );
             }
             return sb.toString();
         } catch (Exception e) {
@@ -112,5 +87,38 @@ public class MerkleTrees {
     public String getRoot() {
         return this.root;
     }
+
+    //reverse input hash value (str)
+    private String rStrInHex(String str){
+        int index = str.length() - 2;
+        StringBuilder rStr = new StringBuilder();
+
+        while (index >= 0){
+            rStr.append(str, index, index + 2);
+            index -= 2;
+        }
+        return rStr.toString();
+    }
+
+    //Decode String hash to byte
+    private byte[] StrInByte(String strInHex){
+
+        int index = 0;
+        int j = 0;
+        byte[] rStrInByte = new byte[strInHex.length()/2];
+        StringBuilder rStrInHex = new StringBuilder();
+
+        while (index < strInHex.length()){
+            String tmp;
+            tmp = strInHex.substring(index ,index+2);
+            rStrInHex.append(tmp);
+
+            int firstDigit = Character.digit(tmp.charAt(0), 16);
+            int secondDigit = Character.digit(tmp.charAt(1), 16);
+            rStrInByte[j++] = (byte) ((firstDigit << 4) + secondDigit);
+            index += 2;
+        }
+
+        return rStrInByte;
+    }
 }
-//https://www.programmersought.com/article/29645319891/
